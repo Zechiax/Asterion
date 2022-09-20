84513b35f207e3e9b1a63d16a00c3080e4e1f3ee
Zechiax <106590288+Zechiax@users.noreply.github.com>
2022-09-20 14:51:32 +0200

    add base class

---
+++ RinthBot/Modules/SettingsModule.cs
@@ -0,0 +1,24 @@
+﻿using Discord.Interactions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using RinthBot.Interfaces;
+
+namespace RinthBot.Modules;
+
+public class SettingsModule : InteractionModuleBase<SocketInteractionContext>
+{
+    private readonly ILogger<SettingsModule> _logger;
+    private readonly IDataService _dataService;
+
+    public SettingsModule(IServiceProvider serviceProvider)
+    {
+        _logger = serviceProvider.GetRequiredService<ILogger<SettingsModule>>();
+        _dataService = serviceProvider.GetRequiredService<IDataService>();
+    }
+
+    [SlashCommand("settings", "Change settings for RinthBot")]
+    public async Task SettingsCommand()
+    {
+        
+    }
+}

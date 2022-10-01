919a935f5f144104c14dccc4fe7e2230a09b4c32
Zechiax <106590288+Zechiax@users.noreply.github.com>
2022-10-01 15:53:52 +0200

    add base message handler

---
+++ RinthBot/Services/MessageHandler.cs
@@ -0,0 +1,21 @@
+﻿using Discord.Interactions;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RinthBot.Interfaces;
+
+namespace RinthBot.Services;
+
+public class MessageHandler
+{
+    private readonly IDataService _dataService;
+    private readonly DiscordSocketClient _client;
+    private readonly InteractionService _interactionService;
+
+    public MessageHandler(IServiceProvider services)
+    {
+        _dataService = services.GetRequiredService<IDataService>();
+        _client = services.GetRequiredService<DiscordSocketClient>();
+        _interactionService = services.GetRequiredService<InteractionService>();
+    }
+}

8946431a21ad23d7450b6cd249f09954ea5e0f0b
Zechiax <106590288+Zechiax@users.noreply.github.com>
2022-12-20 18:49:17 +0100

    add migration

---
+++ Asterion/Migrations/20221220172041_addSubscribeButtonOption.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Asterion.Migrations
+{
+    /// <inheritdoc />
+    public partial class addSubscribeButtonOption : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ShowSubscribeButton",
+                table: "GuildSettings",
+                type: "INTEGER",
+                nullable: true,
+                defaultValue: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ShowSubscribeButton",
+                table: "GuildSettings");
+        }
+    }
+}

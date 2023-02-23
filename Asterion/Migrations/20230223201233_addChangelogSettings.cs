db9c3c3d27e4985ae9c9b5530fde60f456d28953
Zechiax <106590288+Zechiax@users.noreply.github.com>
2023-02-23 21:13:31 +0100

    correctly add migration

---
+++ Asterion/Migrations/20230223201233_addChangelogSettings.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Asterion.Migrations
+{
+    /// <inheritdoc />
+    public partial class addChangelogSettings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

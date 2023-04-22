cca3d6e8fdf4e146c991bfe9159b913f91192c35
Zechiax <106590288+Zechiax@users.noreply.github.com>
2023-04-23 00:31:08 +0200

    base for implementation

--- Asterion/Modules/GraphModule.cs
+++ Asterion/Modules/GraphModule.cs
@@ -1,4 +1,5 @@
 using Asterion.Services;
+using Asterion.Services.Modrinth;
 using Discord.Interactions;
 
 namespace Asterion.Modules;
@@ -6,15 +7,32 @@ namespace Asterion.Modules;
 public class GraphModule : InteractionModuleBase<SocketInteractionContext>
 {
     private readonly DownloadManager _downloadManager;
+    private readonly ModrinthService _modrinthService;
     
-    public GraphModule(DownloadManager downloadManager)
+    public GraphModule(DownloadManager downloadManager, ModrinthService modrinthService)
     {
         _downloadManager = downloadManager;
+        _modrinthService = modrinthService;
     }
     
-    [SlashCommand("graph", "Graphs the downloads of a mod", runMode: RunMode.Async)]
-    public async Task Graph()
+    [SlashCommand("chart", "Graphs the downloads of a mod", runMode: RunMode.Async)]
+    public async Task Chart(string projectId = "fabric-api")
     {
+        await DeferAsync();
         
+        var mod = await _modrinthService.FindProject(projectId);
+        if (mod.Success == false)
+        {
+            await FollowupAsync("Project not found", ephemeral: true);
+            return;
+        }
+
+        var downloadData = await _downloadManager.GetTotalDownloadsAsync(mod.Payload.Project.Id);
+        
+        if (downloadData.Count == 0)
+        {
+            await FollowupAsync("No downloads are currently stored", ephemeral: true);
+            return;
+        }
     }
 }

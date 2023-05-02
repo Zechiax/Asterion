ea4561682e350619993d84938aeb129b28fbae82
Zechiax <106590288+Zechiax@users.noreply.github.com>
2023-05-02 16:22:55 +0200

    make list command use embeds to paginate the subscribed commands

---
+++ Asterion/DiscordEmbedConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace Asterion;
+
+public class DiscordEmbedConstants
+{
+    public static int MaxEmbedDescriptionLength = 4096;
+    public static int MaxEmbedTitleLength = 256;
+    public static int MaxEmbedFieldCount = 25;
+    public static int MaxEmbedFieldNameLength = 256;
+    public static int MaxEmbedFieldValueLength = 1024;
+    public static int MaxEmbedFooterTextLength = 2048;
+    public static int MaxEmbedAuthorNameLength = 256;
+}

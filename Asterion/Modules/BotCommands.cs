2fc8a7480ca084b58280af153bd2a064d4f8b7ca
Zechiax <106590288+Zechiax@users.noreply.github.com>
2023-06-12 11:33:52 +0200

    ping command for debugging only

--- Asterion/Modules/BotCommands.cs
+++ Asterion/Modules/BotCommands.cs
@@ -4,10 +4,12 @@ namespace Asterion.Modules;
 
 public class BotCommands : InteractionModuleBase<SocketInteractionContext>
 {
+#if DEBUG
     [SlashCommand("ping", "Pings the bot", runMode: RunMode.Async)]
     public async Task Ping()
     {
         await RespondAsync($"Pong :ping_pong: It took me {Context.Client.Latency}ms to respond to you",
             ephemeral: true);
     }
+#endif
 }

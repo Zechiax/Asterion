1ec205155c920f900646541d92e393a0a1e32751
Zechiax <106590288+Zechiax@users.noreply.github.com>
2024-04-18 20:25:29 +0200

    add custom ping role to db model

---
+++ Asterion/Migrations/20240418182429_add custom ping role.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Asterion.Migrations
+{
+    /// <inheritdoc />
+    public partial class addcustompingrole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<ulong>(
+                name: "CustomPingRole",
+                table: "ModrinthEntries",
+                type: "INTEGER",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CustomPingRole",
+                table: "ModrinthEntries");
+        }
+    }
+}
